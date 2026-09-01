3e48f2fe034826438b71a19f2a87254ab4de9749
Pranav P <74554594+pranavp311@users.noreply.github.com>
2026-09-01 22:11:54 +0800

    Complete Part 3

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,4 +1,9 @@
+import java.util.Collections;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -17,5 +22,22 @@ public class DialogBox extends HBox {
         displayPicture.setFitHeight(100.0);
         setAlignment(Pos.TOP_RIGHT);
         getChildren().addAll(text, displayPicture);
+    }
+
+    private void flip() {
+        ObservableList<Node> children = FXCollections.observableArrayList(getChildren());
+        Collections.reverse(children);
+        getChildren().setAll(children);
+        setAlignment(Pos.TOP_LEFT);
+    }
+
+    public static DialogBox getUserDialog(String message, Image image) {
+        return new DialogBox(message, image);
+    }
+
+    public static DialogBox getDukeDialog(String message, Image image) {
+        DialogBox dialogBox = new DialogBox(message, image);
+        dialogBox.flip();
+        return dialogBox;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     private final Image userImage = new Image(getClass().getResourceAsStream("/images/DaUser.png"));
+    private final Image dukeImage = new Image(getClass().getResourceAsStream("/images/DaDuke.png"));
+    private final Duke duke = new Duke();
 
     private ScrollPane scrollPane;
     private VBox dialogContainer;
@@ -25,7 +27,6 @@ public class Main extends Application {
 
         userInput = new TextField();
         sendButton = new Button("Send");
-        dialogContainer.getChildren().add(new DialogBox("Hello!", userImage));
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
@@ -52,7 +53,20 @@ public class Main extends Application {
         AnchorPane.setLeftAnchor(userInput, 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
 
+        sendButton.setOnMouseClicked(event -> handleUserInput());
+        userInput.setOnAction(event -> handleUserInput());
+        dialogContainer.heightProperty().addListener(observable -> scrollPane.setVvalue(1.0));
+
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String dukeText = duke.getResponse(userText);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getDukeDialog(dukeText, dukeImage));
+        userInput.clear();
     }
 }
